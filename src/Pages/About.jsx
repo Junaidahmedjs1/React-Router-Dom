e2af0f76dcc38946fd6ab13259bbb0d69e3eacf4
junaidahmedjs1 <Junaidahmed31254@gmail.com>
2025-04-07 13:15:25 +0500

    React Router Dom

---
+++ src/Pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+    <h1>About</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, magnam illum quis eius et ut molestiae eum expedita laboriosam quia dolor aspernatur exercitationem distinctio quam harum consequatur aperiam molestias suscipit ipsam perspiciatis vitae quisquam assumenda provident. Quis non nobis at voluptatibus consectetur corrupti suscipit eligendi? Reiciendis aspernatur iste quos voluptatem quisquam molestias minus, aliquid modi delectus sint, veniam facilis doloribus! Odit veritatis excepturi mollitia dolorem aperiam quas voluptates ipsa modi magni debitis amet, consequuntur perferendis deleniti sint inventore provident quidem, culpa facere aspernatur, maxime placeat eaque. Laudantium molestiae maxime, esse culpa vitae soluta error, est corrupti excepturi ipsam ea obcaecati tenetur, et dolorum vel! Iste sed ab rerum ducimus ea totam delectus amet id animi reprehenderit, fuga vero voluptate consectetur? Rerum placeat animi illum provident asperiores delectus, incidunt maiores ipsa praesentium quasi corrupti quae accusantium iusto aperiam necessitatibus blanditiis minima tempore ad. Corrupti, dicta qui. Culpa odit voluptatibus quam, sunt dolore ea error impedit fugiat sequi eos unde excepturi delectus non aut placeat ex obcaecati, iure harum quos soluta quo ipsum expedita saepe. Deleniti beatae et voluptatibus porro aspernatur debitis perspiciatis nemo, expedita officia praesentium dignissimos pariatur doloribus animi suscipit dolorem mollitia, sed sequi velit molestias at? Aperiam, harum cum.</p>
+    </>
+  )
+}
+
+export default About
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <h1>Home</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, reprehenderit! Consequatur necessitatibus nostrum voluptates labore aperiam, id illo totam, debitis modi similique inventore vel cum quos, sequi voluptatibus eligendi sit. Reiciendis, expedita laboriosam saepe quod vitae modi similique necessitatibus ullam harum delectus praesentium illo qui officia beatae esse exercitationem possimus repudiandae totam. Sequi hic voluptate sed fuga! Enim quasi cupiditate dolore soluta architecto, in voluptatum accusantium ea doloribus odit harum rem qui, et obcaecati, blanditiis provident veritatis facere. Illum magni iste incidunt sint! Atque, similique eveniet ipsum voluptates ab nobis placeat ducimus facere inventore alias, aliquam eius eaque doloribus dolorem dolore quae incidunt. Tenetur in accusamus, dicta corrupti ipsum quos nisi maiores! Aperiam delectus laboriosam ipsa, facilis minus consequuntur possimus laudantium amet! Nesciunt, itaque in? Magni nesciunt ullam, rerum nam culpa quos eaque, laborum ipsum quas reprehenderit cumque. Ipsam commodi earum voluptatem excepturi quibusdam saepe delectus sint quod aut quas tempore laborum, architecto animi at omnis, beatae, ea eos eaque dignissimos harum. Voluptatibus minima ducimus facere animi ipsa atque in. Sed ea omnis similique voluptates magni, quis, nobis et, dolorem cumque consequatur praesentium corrupti ex doloribus architecto mollitia rerum dolore! Vero veritatis deleniti, molestias ipsum quia dicta magnam vel temporibus beatae voluptate magni a? Illum corporis et veritatis, suscipit ipsa ratione voluptatum tenetur vitae obcaecati iste, esse rerum laboriosam asperiores laudantium sit ducimus voluptatem, minima in consectetur tempore est distinctio? Recusandae in animi minima dicta vel laboriosam quis et eaque quo eveniet ut dolorem, perspiciatis distinctio impedit corrupti deleniti ab molestiae odio tenetur. Officiis fuga blanditiis sed eaque earum repellat eum, vitae adipisci. Numquam asperiores commodi beatae voluptatem vero minus inventore nam at sit similique, deserunt maiores necessitatibus. Porro at quia suscipit eveniet iusto omnis atque impedit optio nisi. Itaque delectus quaerat non perspiciatis sequi veniam expedita, corrupti voluptas harum.
+    </p>
+    </>
+  )
+}
+
+export default Home
